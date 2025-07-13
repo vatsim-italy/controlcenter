39c9775c8a789666f9f35d19f9fb8eb0f93ed3a7
FabioMike06 <72151591+FabioMike06@users.noreply.github.com>
2025-07-13 14:37:19 +0200

    @TomDS66 rompe tutto

--- app/Notifications/TrainingMentorNotification.php
+++ app/Notifications/TrainingMentorNotification.php
@@ -47,7 +47,7 @@ class TrainingMentorNotification extends Notification implements ShouldQueue
     {
         $textLines = [
             "It's your turn! You've been assigned a mentor for your training: " . $this->training->getInlineRatings() . ' in ' . Area::find($this->training->area_id)->name . '.',
-            'Your mentor is: **' . $this->training->getInlineMentors() . '**. You can contact them through the message system at forums or search them up on [Discord](' . Setting::get('linkDiscord') . ').',
+            'Your mentor is: **' . $this->training->getInlineMentors() . '**. You can contact them through [Discord](' . Setting::get('linkDiscord') . ').',
             'If you do not contact your mentor within 7 days, your training request will be closed and you lose the place in the queue.',
         ];
 
